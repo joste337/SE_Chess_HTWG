96ecce1a4cbc3f6d811a8d05946add7f93bf03fe
Jörg Stenger <joergstenger@exozet.com>
2019-01-19 18:23:36 +0100

    Added tests for FileIOs

---
+++ src/test/scala/de/htwg/se/SE_Chess_HTWG/model/fileIOComponent/FileIOJsonSpec.scala
@@ -0,0 +1,24 @@
+package de.htwg.se.SE_Chess_HTWG.model.fileIOComponent
+
+import de.htwg.se.SE_Chess_HTWG.controller.GameStatus
+import de.htwg.se.SE_Chess_HTWG.model.gridComponent.GridInterface
+import de.htwg.se.SE_Chess_HTWG.testUtil.TestGrid
+import org.junit.runner.RunWith
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class FileIOJsonSpec extends WordSpec with Matchers {
+  val testGrid: GridInterface = TestGrid.getTestGrid
+  val fileIO: FileIOJsonImpl = new FileIOJsonImpl
+
+  "A grid" when { "saved and loaded via JSON" should {
+    fileIO.save(testGrid, GameStatus.PLAYER1TURN)
+    val loadedGrid = fileIO.load._1
+
+    "load correctly" in {
+      loadedGrid.toString.equals(testGrid.toString) should be(true)
+    }
+  }}
+}
